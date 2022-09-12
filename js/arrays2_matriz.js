d3ecda21db559f76dc536da5b121e1dd1180e899
guilherme maywald <guilhermemaywald@gmail.com>
2022-09-12 08:09:39 -0300

    Atividade Pratica 9 JS

---
+++ js/arrays2_matriz.js
@@ -0,0 +1,5 @@
+{
+    const arrNumero = [[2,0,0], [2,1,2], [2,2,4], [2,3,6], [2,4,8], [2,5,10], [2,6,12], [2,7,14], [2,8,16], [2,9,18], [2,10,20]];
+    alert('Matriz é uma tabela organizada em linhas e colunas no formato m x n, onde m representa o número de linhas (horizontal) e n o número de colunas (vertical).');
+    alert('Matriz é uma tabela multidimensional que guarda elementos. Muito utilizada na metamática mas também muito útil em várias áreas diferentes.');
+}
